9cea213befdce76103a5600070fd43114112d283
Christopher Guess <cguess@gmail.com>
2020-02-17 18:50:57 -0500

    Messed up getting environment properly

--- config/initializers/default_url_options.rb
+++ config/initializers/default_url_options.rb
@@ -2,7 +2,7 @@
 # assumed to be a Heroku review app. Note that `Hash#fetch` is used defensively
 # so the app will blow up at boot-time if both `DEFAULT_URL_HOST` and
 # `HEROKU_APP_NAME` aren't defined.
-if Figaro.env.RAILS_ENV == "production"
+if Rails.env == "production"
   host = ENV["HOST_NAME"] ||
     "#{ENV.fetch('HEROKU_APP_NAME')}.herokuapp.com"
 
