ae7e93378989725ed6af4f0bff708b29f12ccd46
Christopher Guess <cguess@gmail.com>
2020-03-10 20:38:48 -0400

    Change name migration to up/down

--- db/migrate/20200305165140_add_name_to_user.rb
+++ db/migrate/20200305165140_add_name_to_user.rb
@@ -1,11 +1,13 @@
 class AddNameToUser < ActiveRecord::Migration[6.0]
-  def change
-    change_table :users do |t|
-      t.string :name
-    end
+  def up
+    add_column :users, :name, :string
 
     # Since we have users already we'll just add Filler Name, default otherwise wouldn't allow us to
     # validate later.
     User.all.each { |u| u.update!({ name: "Filler Name" }) }
+  end
+
+  def down
+    remove_column :users, :name, :string
   end
 end
