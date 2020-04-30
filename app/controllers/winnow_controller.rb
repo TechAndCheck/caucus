d8000ecc4716b06bb96f7dab3e9457ba3e14cac4
Justin Reese <justin@justinreese.com>
2020-04-30 16:30:28 -0500

    Gracefully handle a lack of winnowable claims
    Before, if there were no winnowable claims to show the user, the app
    just crashed.
    
    Now, we redirect to the home with a happy notice.
    
    This commit also moves the `authenticate_user!` action to the top of
    the list, so it’s performed first. (Otherwise, we’ll run the other two
    actions before authenticating the user.)
    
    Affects #41
    
    Signed-off-by: Justin Reese <justin@justinreese.com>

--- app/controllers/winnow_controller.rb
+++ app/controllers/winnow_controller.rb
@@ -1,9 +1,9 @@
 class WinnowController < ApplicationController
-  before_action :verify_claim, except: [:index]
   before_action :authenticate_user!
+  before_action :verify_winnowable_claim, only: [:index]
+  before_action :verify_claim, except: [:index]
 
   def index
-    @claim = winnowable_claim
     # We only want categories not already assigned to the claim
     @categories = Category.all.order("name ASC") - @claim.categories
     turbolinks_animate "slideInRight"
@@ -31,6 +31,13 @@ private
     @claim = Claim.find(claim_params[:id])
   rescue ActiveRecord::RecordNotFound
     render file: "#{Rails.root}/public/404.html", layout: false, status: :not_found
+  end
+
+  def verify_winnowable_claim
+    @claim = winnowable_claim
+    if @claim.nil?
+      redirect_to root_path, flash: { notice: "Looks like there are no claims to winnow. Go do something else fun!" }
+    end
   end
 
   def claim_params
